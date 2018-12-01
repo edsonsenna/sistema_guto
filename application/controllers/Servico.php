769237babc8a562cd08e31b6cb24fb3943f4d878
Edson de Senna Junior <edsonsjr14@outlook.com>
2018-11-30 23:17:01 -0200

    Reducao de Codigo Inclusao Mes

--- application/controllers/Servico.php
+++ application/controllers/Servico.php
@@ -38,186 +38,46 @@ class Servico extends CI_Controller {
         $date_end = DateTime::createFromFormat($format, $string_date_end);
 
         $num_dias = $this->input->post('dias_semana');
-        $seg = $this->input->post('dia_seg');
-        $ter = $this->input->post('dia_ter');
-        $qua = $this->input->post('dia_qua');
-        $qui = $this->input->post('dia_qui');
-        $sex = $this->input->post('dia_sex');
-        $sab = $this->input->post('dia_sab');
-        $dom = $this->input->post('dia_dom');
-
+        $array_dias = [];
+        if($this->input->post('dia_seg') != null) { array_push($array_dias, 'Mon');}
+        if($this->input->post('dia_ter') != null) { array_push($array_dias, 'Tue');}
+        if($this->input->post('dia_qua') != null) { array_push($array_dias, 'Wed');}
+        if($this->input->post('dia_qui') != null) { array_push($array_dias, 'Thu');}
+        if($this->input->post('dia_sex') != null) { array_push($array_dias, 'Fri');}
+        if($this->input->post('dia_sab') != null) { array_push($array_dias, 'Sat');}
+        if($this->input->post('dia_dom') != null) { array_push($array_dias, 'Sun');}
+      
         $data_inicio = $date_start;
 
-        //var_dump($date_start);
 
         $data_base = new DateTime($data_inicio->format('Y').'-'.$data_inicio->format('m').'-01');
         $hora_base_inicio = $this->input->post('inicio');
         $hora_base_fim = $this->input->post('fim');
         
         $data_ger = get_object_vars($date_start)['date'];
-        /*
-        var_dump($ter);
-        var_dump($qua);
-        var_dump($data_base->format('D'));
-        $data_base->modify('+1 day');
-        var_dump($data_base);
-        while(intval(substr($data_ger,5, 6)) == $data_base->format('m')){
-            var_dump($data_base);
-            if(strcmp($ter, $data_base->format('D') == 0)) { echo 'Terça';}
-            if(strcmp($qui, $data_base->format('D') == 0)) { echo 'Quinta';}
-            $data_base->modify('+1 day');
-        }
-        */
-
         $mes = intval(substr($data_ger,5, 6));
-        /*
-        var_dump($mes);
-        var_dump($mes == $data_base->format('m'));
-        $data_base->modify('+1 month');
-        var_dump($mes == $data_base->format('m'));
-        var_dump($seg);
-        var_dump($data_base->format('D'));
-        var_dump(strcmp($seg, 'Mon'));
-        */
+
         while($mes == $data_base->format('m')){
-            if(strcmp($seg, $data_base->format('D')) == 0 ){
-                $dados = array(
-                    "id_cliente" => intval($this->input->post('cliente')),
-                    "tipo_servico" => intval($this->input->post('tipo_servico')),
-                    "id_usuario" => 1,
-                    "pago_servico" => 1,
-                    "id_equipamento" => intval($this->input->post('equip')),
-                    "desconto_servico" => doubleval($this->input->post('desconto')), 
-                    "data_inicio_servico" => $data_base->format('Y-m-d').' '.$hora_base_inicio,
-                    "data_vencimento_servico" =>  $data_base->format('Y-m-d').' '.$hora_base_fim,
-                    "nome_servico" => $this->input->post('desc'),
-                    "desc_servico" => $this->input->post('desc')
-        
-                );
+            $dados = array(
+                "id_cliente" => intval($this->input->post('cliente')),
+                "tipo_servico" => intval($this->input->post('tipo_servico')),
+                "id_usuario" => 1,
+                "pago_servico" => 1,
+                "id_equipamento" => intval($this->input->post('equip')),
+                "desconto_servico" => doubleval($this->input->post('desconto')), 
+                "data_inicio_servico" => $data_base->format('Y-m-d').' '.$hora_base_inicio,
+                "data_vencimento_servico" =>  $data_base->format('Y-m-d').' '.$hora_base_fim,
+                "nome_servico" => $this->input->post('desc'),
+                "desc_servico" => $this->input->post('desc')
+    
+            );
 
+            if(in_array($data_base->format('D'), $array_dias)){
                 $this->Servicos_model->add('servico', $dados);
-
+                $data_base->modify('+1 day');
+            }else{
                 $data_base->modify('+1 day');
             }
-            else if(strcmp($ter, $data_base->format('D')) == 0 ){
-                $dados = array(
-                    "id_cliente" => intval($this->input->post('cliente')),
-                    "tipo_servico" => intval($this->input->post('tipo_servico')),
-                    "id_usuario" => 1,
-                    "pago_servico" => 1,
-                    "id_equipamento" => intval($this->input->post('equip')),
-                    "desconto_servico" => doubleval($this->input->post('desconto')), 
-                    "data_inicio_servico" => $data_base->format('Y-m-d').' '.$hora_base_inicio,
-                    "data_vencimento_servico" =>  $data_base->format('Y-m-d').' '.$hora_base_fim,
-                    "nome_servico" => $this->input->post('desc'),
-                    "desc_servico" => $this->input->post('desc')
-        
-                );
-
-                $this->Servicos_model->add('servico', $dados);
-
-                $data_base->modify('+1 day');
-            }
-            else if(strcmp($qua, $data_base->format('D')) == 0  ){
-                $dados = array(
-                    "id_cliente" => intval($this->input->post('cliente')),
-                    "tipo_servico" => intval($this->input->post('tipo_servico')),
-                    "id_usuario" => 1,
-                    "pago_servico" => 1,
-                    "id_equipamento" => intval($this->input->post('equip')),
-                    "desconto_servico" => doubleval($this->input->post('desconto')), 
-                    "data_inicio_servico" => $data_base->format('Y-m-d').' '.$hora_base_inicio,
-                    "data_vencimento_servico" =>  $data_base->format('Y-m-d').' '.$hora_base_fim,
-                    "nome_servico" => $this->input->post('desc'),
-                    "desc_servico" => $this->input->post('desc')
-        
-                );
-
-                $this->Servicos_model->add('servico', $dados);
-
-                $data_base->modify('+1 day');
-            }
-            else if(strcmp($qui, $data_base->format('D')) == 0 ){
-                $dados = array(
-                    "id_cliente" => intval($this->input->post('cliente')),
-                    "tipo_servico" => intval($this->input->post('tipo_servico')),
-                    "id_usuario" => 1,
-                    "pago_servico" => 1,
-                    "id_equipamento" => intval($this->input->post('equip')),
-                    "desconto_servico" => doubleval($this->input->post('desconto')), 
-                    "data_inicio_servico" => $data_base->format('Y-m-d').' '.$hora_base_inicio,
-                    "data_vencimento_servico" =>  $data_base->format('Y-m-d').' '.$hora_base_fim,
-                    "nome_servico" => $this->input->post('desc'),
-                    "desc_servico" => $this->input->post('desc')
-        
-                );
-
-                $this->Servicos_model->add('servico', $dados);
-
-                $data_base->modify('+1 day');
-            }
-            else if(strcmp($sex, $data_base->format('D')) == 0 ){
-                $dados = array(
-                    "id_cliente" => intval($this->input->post('cliente')),
-                    "tipo_servico" => intval($this->input->post('tipo_servico')),
-                    "id_usuario" => 1,
-                    "pago_servico" => 1,
-                    "id_equipamento" => intval($this->input->post('equip')),
-                    "desconto_servico" => doubleval($this->input->post('desconto')), 
-                    "data_inicio_servico" => $data_base->format('Y-m-d').' '.$hora_base_inicio,
-                    "data_vencimento_servico" =>  $data_base->format('Y-m-d').' '.$hora_base_fim,
-                    "nome_servico" => $this->input->post('desc'),
-                    "desc_servico" => $this->input->post('desc')
-        
-                );
-
-                $this->Servicos_model->add('servico', $dados);
-
-                $data_base->modify('+1 day');
-            }
-            else if(strcmp($sab, $data_base->format('D')) == 0 ){
-                $dados = array(
-                    "id_cliente" => intval($this->input->post('cliente')),
-                    "tipo_servico" => intval($this->input->post('tipo_servico')),
-                    "id_usuario" => 1,
-                    "pago_servico" => 1,
-                    "id_equipamento" => intval($this->input->post('equip')),
-                    "desconto_servico" => doubleval($this->input->post('desconto')), 
-                    "data_inicio_servico" => $data_base->format('Y-m-d').' '.$hora_base_inicio,
-                    "data_vencimento_servico" =>  $data_base->format('Y-m-d').' '.$hora_base_fim,
-                    "nome_servico" => $this->input->post('desc'),
-                    "desc_servico" => $this->input->post('desc')
-        
-                );
-
-                $this->Servicos_model->add('servico', $dados);
-
-                $data_base->modify('+1 day');
-            }
-            else if(strcmp($dom, $data_base->format('D')) == 0 ){
-                $dados = array(
-                    "id_cliente" => intval($this->input->post('cliente')),
-                    "tipo_servico" => intval($this->input->post('tipo_servico')),
-                    "id_usuario" => 1,
-                    "pago_servico" => 1,
-                    "id_equipamento" => intval($this->input->post('equip')),
-                    "desconto_servico" => doubleval($this->input->post('desconto')), 
-                    "data_inicio_servico" => $data_base->format('Y-m-d').' '.$hora_base_inicio,
-                    "data_vencimento_servico" =>  $data_base->format('Y-m-d').' '.$hora_base_fim,
-                    "nome_servico" => $this->input->post('desc'),
-                    "desc_servico" => $this->input->post('desc')
-        
-                );
-
-                $this->Servicos_model->add('servico', $dados);
-
-                $data_base->modify('+1 day');
-            }else
-            {
-                $data_base->modify('+1 day');
-            }
-
-
         }
 
         $data['message_error'] = '<div class="alert alert-success alert-dismissible show" role="alert">
@@ -228,43 +88,7 @@ class Servico extends CI_Controller {
         </div>';
         $this->load->view('calendar', $data);
         
-         //////////////////////////////////////
-        /*
-        $dados = array(
-            "id_cliente" => intval($this->input->post('cliente')),
-            "tipo_servico" => intval($this->input->post('tipo_servico')),
-            "id_usuario" => 1,
-            "pago_servico" => 1,
-            "id_equipamento" => intval($this->input->post('equip')),
-            "desconto_servico" => doubleval($this->input->post('desconto')), 
-            "data_inicio_servico" => get_object_vars($date_start)['date'],
-            "data_vencimento_servico" => get_object_vars($date_end)['date'],
-            "nome_servico" => $this->input->post('desc'),
-            "desc_servico" => $this->input->post('desc')
 
-        );
-
-  
-        
-
-        if ($this->Servicos_model->add('servico', $dados)) {
-            $data['message_error'] = '<div class="alert alert-success alert-dismissible show" role="alert">
-            Serviço cadastrado com sucesso no BD!
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>';
-            $this->load->view('calendar', $data);
-        } else {
-            $data['message_error'] = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-            Falha ao cadastrar Serviço no BD!
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>';
-            $this->load->view('calendar', $data);
-        }
-        */
 
        
     }
